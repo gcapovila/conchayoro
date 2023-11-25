16331bc9b372c692ab3e43e79a8fe2e2b8f695d2
gapovila@hotmail.com <gapovila@htoamil.com>
2023-11-25 01:05:27 +0000

    teste2

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono
       text-sm lg:flex">
         <h1> Welcome to the ConchaYOro App </h1>
+        <h2> Developed by Capovila, Garbiel </h2>
+        <p> As a test for his course </p>
       </div>
     </main>
   )
